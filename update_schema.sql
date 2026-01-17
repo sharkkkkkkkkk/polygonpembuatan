b39838518a43470721041ec97dbbac5ad3bd53d5
Afif Ganteng <afifganteng@example.com>
2026-01-18 00:14:50 +0700

    Remove SQL files and update gitignore

--- update_schema.sql
+++
@@ -1,9 +0,0 @@
--- Add new columns to users table
-alter table users 
-add column if not exists name text,
-add column if not exists whatsapp text;
-
--- Update seed admin if needed (optional)
-update users 
-set name = 'Super Admin', whatsapp = '628123456789' 
-where email = 'admin@landscaler.com';
